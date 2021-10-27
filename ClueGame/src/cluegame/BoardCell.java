e75ad3ce27ae6985f2cac7a89d7ccf048e228f5b
sg-fernando <fernando.sandoval.garza@gmail.com>
2021-10-27 17:23:44 -0600

    dsa

--- ClueGame/src/cluegame/BoardCell.java
+++ ClueGame/src/cluegame/BoardCell.java
@@ -35,11 +35,7 @@ public class BoardCell
 	public int getCol() { return this.col; }
 	
 	public Set<BoardCell> getAdjList() { return adjList; }
-	public void addAdjacency(BoardCell cell)
-	{
-		adjList.add(cell);
-	}
-	
+	public void addAdjacency(BoardCel
 	public Boolean isOccupied() { return this.isOccupied; }
 	public void setOccupied(Boolean isOccupied)
 	{
